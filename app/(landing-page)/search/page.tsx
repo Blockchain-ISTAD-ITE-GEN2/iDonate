e8509eeddcb7e5aefb25cbac959a0e377f1a2ed3
kevin-kang-tido <kaykang84@gmail.com>
2024-12-26 00:25:47 +0700

    save change before revert

--- app/(landing-page)/search/page.tsx
+++ app/(landing-page)/search/page.tsx
@@ -1,23 +1,12 @@
-import { SearchPage } from "@/components/search/SearchPage";
+
+import SearchOnPageComponent from "@/components/search/SearchOnPageComponent";
 
 export default function Events() {
-  return (
-    <section className="flex flex-col py-9 gap-9">
-      <div
-        lang="km"
-        className="w-full flex flex-col items-center justify-center gap-2"
-      >
-        <h2 className="text-iDonate-navy-primary text-heading-one-khmer">
-          ស្វែករក កម្មវិធីបរិច្ចាគដែលអ្នកពេញចិត្ត
-        </h2>
-        <h4 className="text-iDonate-navy-primary text-medium-khmer">
-          {" "}
-          ស្វែករកម្មបវិធី វៃអង្គាសដោយ ឈ្មោះនៃអង្កការភាព​ ប្រភេទ និង
-          កាលបរិច្ឆេទ្នាំនៃកម្មវិធី
-        </h4>
-      </div>
-
-      <SearchPage />
-    </section>
-  );
-}
+    return (
+        <>
+            <section>
+                <SearchOnPageComponent/>
+            </section>
+        </>
+    );
+  }
---
+++ components/search/SearchBar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import { Input } from "@/components/ui/input";
+import { cn } from "@/lib/utils";
+import { Search } from "lucide-react";
+
+export function SearchBar({
+                                placeholder = "ស្វែងរក",
+                                className,
+                                ...props
+                            }: React.ComponentPropsWithoutRef<typeof Input>) {
+    return (
+        <form lang={"km"} className="ml-[100px] w-[690px] h-[59px]">
+            <label
+                htmlFor="default-search"
+                className="mb-2 text-[20px]  text-idonate-navy-primary sr-only dark:text-white"
+            >
+                Search
+            </label>
+            <div className="relative">
+                <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+                    <Search className="w-4 h-4 text-gray-500 dark:text-gray-400" />
+                </div>
+                <Input
+                    id="default-search"
+                    type="search"
+                    placeholder={placeholder}
+                    className={cn(
+                        "ps-10", // Adjust padding-left for the search icon
+                        className
+                    )}
+                    {...props}
+                />
+            </div>
+        </form>
+    );
+}
