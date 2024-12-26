63ce621cb8e2e09de4846ff9d8dde1b0b1326462
kevin-kang-tido <kaykang84@gmail.com>
2024-12-26 10:52:28 +0700

    working Github Actions

--- app/(landing-page)/search/page.tsx
+++ app/(landing-page)/search/page.tsx
@@ -4,6 +4,7 @@ import SearchOnPageComponent from "@/components/search/SearchOnPageComponent";
 export default function Events() {
     return (
         <>
+            {/*search page*/}
             <section>
                 <SearchOnPageComponent/>
             </section>
