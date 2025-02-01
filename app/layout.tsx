b16327d34edd762c68bc3581da15f8fa1291cb42
kevin-kang-tido <kaykang84@gmail.com>
2025-02-01 22:00:40 +0700

    Change side the image

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,8 +35,15 @@ export const metadata: Metadata = {
       url: "https://idata.istad.co",
       type: "website",
       locale: "kh_KH",
-      images: "https://idonateapi.kangtido.life/media/a1108ea2-b140-4a4c-b07d-21aa141853f5.png",
-  }
+      images: [
+        {
+            url: "https://idonateapi.kangtido.life/media/287b8ea2-c365-42e0-8683-57f57f2d5e78.png",
+            width: 1200,  
+            height: 630,
+            alt: "iDonate Banner"
+        }
+    ],
+    }
 };
 
 const inter = localFont({
