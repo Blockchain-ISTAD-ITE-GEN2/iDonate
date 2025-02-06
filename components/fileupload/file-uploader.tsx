b0ac19cd841b34088f119b2b9cf930f6a2dc4e08
Dark Fighter <sanhpanha3003@gmail.com>
2025-02-06 10:16:41 +0700

    Create event with image success

--- components/fileupload/file-uploader.tsx
+++ components/fileupload/file-uploader.tsx
@@ -28,7 +28,7 @@ export function FileUploader(props: FileUploaderProps) {
   } = props;
 
   const [files, setFiles] = useControllableState({
-    prop: valueProp,
+    prop: Array.isArray(valueProp) ? valueProp : valueProp ? [valueProp] : undefined,
     onChange: onValueChange,
   });
 
